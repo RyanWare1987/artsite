19b0881d2f820c6896521706e5ffb12eba16e1fb
Ryan Ware <ryan.ware1987@gmail.com>
2018-06-09 17:57:17 +0100

    added view & url for about page

--- artsite/urls.py
+++ artsite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from home import views
+from .views import about
 from accounts.views import login, logout, register, profile, image_form_upload
 from django.conf import settings 
 from gallery.views import edit_profile, gallery
@@ -26,11 +27,11 @@ from artwork.views import productsall
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/$', views.index, name='home'),
+    url(r'^about/$', about, name='about'),
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
-    #url(r'^about/$', about, name='about'),
     url(r'^gallery/$', gallery, name='gallery'),   #old gallery
     url(r'^edit_profile/$', edit_profile, name='edit_profile'),
     url(r'^image_form_upload/$', image_form_upload, name='image_form_upload'),
