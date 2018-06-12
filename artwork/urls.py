c2c0f32ccac8a4c2cbcea306426ed8798962b9c5
Ryan Ware <ryan.ware1987@gmail.com>
2018-06-12 08:19:11 +0100

    fixed display of products in artwork

--- artwork/urls.py
+++ artwork/urls.py
@@ -6,16 +6,13 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^home/$', views.index, name='home'),
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, name='logout'),
     #url(r'^register/$', register, name='register'),
     #url(r'^profile/$', profile, name='profile'),
     #url(r'^image_form_upload/$', image_form_upload, name='image_form_upload'),
+    url(r'^products/(?P<id>\d+)/$', productdetail),
     url(r'^products/$', productsall, name='products'),
     #url(r'^productdetail/$', productdetail, name='productdetail'),
-    url(r'^(?P<id>\d+)/$', productdetail),
 
 
 
