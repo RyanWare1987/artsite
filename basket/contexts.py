5d480127cb970233517c9b227b2f72e153018481
Ryan Ware <ryan.ware1987@gmail.com>
2018-06-13 18:26:48 +0100

    added basket app to handle payments

---
+++ basket/contexts.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404
+from artwork.models import Product
+
+
+def basket_contents(request):
+    """
+    Ensures that the basket contents are available when rendering every page
+    """
+    
+    basket = request.session.get('basket', {})
+    
+    basket_items = []
+    total = 0
+    product_count = 0
+    for id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({'id':id, 'quantity': quantity, 'product': product})
+        
+    return { 'basket_items': basket_items, 'total': total, 'product_count': product_count }
+    
