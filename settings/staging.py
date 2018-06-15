4e5fc5310ae9e51df0e3f9a326a9ed25c3f0218c
RyanWare1987 <ryan.ware1987@gmail.com>
2018-06-15 23:27:06 +0100

    edited database to use postgres in heroku

--- settings/staging.py
+++ settings/staging.py
@@ -1,19 +1,12 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'artpictures',
-        # These are the login details for MySQL:
-        #'USER': '',
-        #'PASSWORD': '',
-    }
+    'default': dj_database_url.config('DATABASE_URL')   
 }
 
 
